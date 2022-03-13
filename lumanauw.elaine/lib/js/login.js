c56d53db23a613bf75b35a748019047bca806a3e
Elaine Lumanauw <elumanau2@art.edu>
2022-03-12 21:09:06 -0800

    real fake login page (it works)

---
+++ lumanauw.elaine/lib/js/login.js
@@ -0,0 +1,34 @@
+
+const checkLoginForm = () => {
+   let user = $("#welcomeback-login-username").val();
+   let pass = $("#welcomeback-login-password").val();
+
+   console.log(user,pass)
+
+   if (user === 'elaine' && pass === 'pass') {
+      // logged in
+      console.log('success');
+      sessionStorage.userId = 3;
+      $("#login-form")[0].reset();
+   } else {
+      // not logged in
+      console.log('failure');
+      sessionStorage.removeItem('userId');
+   }
+
+   checkUserId();
+}
+
+const checkUserId = () => {
+   let p = ["#login-page","#signup-page",""];
+
+   if (sessionStorage.userId === undefined) {
+      // not logged in
+      if(!p.some(o => o === window.location.hash))
+         $.mobile.navigate("#signup-page");
+   } else {
+      // logged in
+      if(p.some(o => o === window.location.hash))
+         $.mobile.navigate("#nav-page");
+   }
+}
