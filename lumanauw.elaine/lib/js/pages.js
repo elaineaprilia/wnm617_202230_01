b1b44698de03bc520c75071ca88d31d85c02650b
Elaine Lumanauw <elumanau2@art.edu>
2022-04-15 15:18:14 -0700

    all js files: pages, parts

--- lumanauw.elaine/lib/js/pages.js
+++ lumanauw.elaine/lib/js/pages.js
@@ -1,4 +1,5 @@
 
+//nav-page
 const NavPage = async() => {
   let {result:bagels} = await query({
   	type:'bagels_by_user_id',
@@ -13,7 +14,18 @@ const NavPage = async() => {
 	$("#nav-page .gallery-control").html(makeBagelList(bagels));
 }
 
+
+//profile-page
 const ProfPage = async() => {
+  let{result:users} = await query({
+    type:'user_by_id',
+    params:[sessionStorage.userId]
+  })
+  let [user] = users;
+
+  console.log(user)
+
+  $("#profile-page .page-control").html(makeUserProfilePage(user));
 
 }
 
--- lumanauw.elaine/lib/js/parts.js
+++ lumanauw.elaine/lib/js/parts.js
@@ -7,3 +7,13 @@ const makeBagelList = templater(o=>`
          <div class="image-nav-1" style="background-image: url(${o.img});">
          </div>
 `);
+
+
+
+
+const makeUserProfilePage = o =>`
+      <img src="${o.img}" id="edit-profilepic-img" /></a>
+      <div class="display-flex flex-column">
+         <h2>@${o.username}</h2>
+      </div>
+`;
